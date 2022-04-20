2836b59003f842b1b9d5893e4fda5258e7fa627e
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-04-20 18:04:56 +0300

    added router to cars for rent page

--- src/app/pages/cars-for-rent/cars-for-rent.page.ts
+++ src/app/pages/cars-for-rent/cars-for-rent.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cars-for-rent',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarsForRentPage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+  GoToCarsForRentMoreInfo(){
+    this.router.navigate(['/cars-for-rent-more-info']);
+  }
+  GoToCarsForRentFilter(){
+    this.router.navigate(['/cars-for-rent-filter']);
+  }
+  GoToHomePage(){
+    this.router.navigate(['/home-page']);
+  }
+  GoToProfile(){
+    this.router.navigate(['/profile']);
+  }
+  GoToFavorites(){
+    this.router.navigate(['/favorites']);
+  }
+  GoToLogIn(){
+    this.router.navigate(['/login']);
   }
 
 }
