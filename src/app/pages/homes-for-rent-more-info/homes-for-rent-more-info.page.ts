3589d1666beb1604775c60851e7bf3f867e1a64f
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-04-20 17:51:08 +0300

    added routers and onclicks to info homes rent

--- src/app/pages/homes-for-rent-more-info/homes-for-rent-more-info.page.ts
+++ src/app/pages/homes-for-rent-more-info/homes-for-rent-more-info.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-homes-for-rent-more-info',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomesForRentMoreInfoPage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+  GoToHomePage(){
+    this.router.navigate(['/home-page']);
+  }
+  GoToProfile(){
+    this.router.navigate(['/profile']);
+  }
+  GoToFavorites(){
+    this.router.navigate(['/favorites']);
+  }
+  GoToLogIn(){
+    this.router.navigate(['/login']);
   }
 
 }
