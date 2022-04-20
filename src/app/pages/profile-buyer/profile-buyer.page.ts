75c896900c52aa54b7b1d73e3551f266bdf78176
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-04-20 18:35:49 +0300

    added router to profile buyer page

--- src/app/pages/profile-buyer/profile-buyer.page.ts
+++ src/app/pages/profile-buyer/profile-buyer.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile-buyer',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileBuyerPage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+  GoToEditProfile(){
+    this.router.navigate(['/edit-profile']);
+  }
+  GoToHomePage(){
+    this.router.navigate(['/home-page']);
+  }
+  GoToProfile(){
+    this.router.navigate(['/profile']);
+  }
+  GoToFavorites(){
+    this.router.navigate(['/favorites']);
+  }
+  GoToLogIn(){
+    this.router.navigate(['/login']);
   }
 
 }
