040cc5fb442cd0388172277ea9d2ed90c1853d87
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-04-20 14:40:38 +0300

    added routers and onclicks to home page

--- src/app/pages/home-page/home-page.page.ts
+++ src/app/pages/home-page/home-page.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePagePage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  GoToCars(){
+    this.router.navigate(['/cars']);
+  }
+  GoToHomesForSale(){
+    this.router.navigate(['/homes-for-sale']);
+  }
+  GoToHomesForRent(){
+    this.router.navigate(['/homes-for-rent']);
+  }
+  GoToHomePage(){
+    this.router.navigate(['/home-page']);
+  }
+  GoToProfile(){
+    this.router.navigate(['/profile']);
+  }
+  GoToFavorites(){
+    this.router.navigate(['/favorites']);
+  }
+  GoToLogIn(){
+    this.router.navigate(['/login']);
   }
 
 }
