41bde5a86e4ef7a6d9a6c800cfbc60fb844120da
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-04-20 18:28:39 +0300

    added router to add home page

--- src/app/pages/add-home/add-home.page.ts
+++ src/app/pages/add-home/add-home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-home',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddHomePage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+  GoToSaleList(){
+    this.router.navigate(['/sales-list']);
+  }
+  GoToHomePage(){
+    this.router.navigate(['/home-page']);
+  }
+  GoToProfile(){
+    this.router.navigate(['/profile']);
+  }
+  GoToFavorites(){
+    this.router.navigate(['/favorites']);
+  }
+  GoToLogIn(){
+    this.router.navigate(['/login']);
   }
 
 }
