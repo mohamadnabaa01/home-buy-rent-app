71fe9bda1df1b2eeea96e06ac26971c6d57add6c
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-04-20 18:26:15 +0300

    added router to add car page

--- src/app/pages/add-car/add-car.page.ts
+++ src/app/pages/add-car/add-car.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-car',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCarPage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
+  GoToSaleList(){
+    this.router.navigate(['/sales-list']);
+  }
+  GoToHomePage(){
+    this.router.navigate(['/home-page']);
+  }
+  GoToProfile(){
+    this.router.navigate(['/profile']);
+  }
+  GoToFavorites(){
+    this.router.navigate(['/favorites']);
+  }
+  GoToLogIn(){
+    this.router.navigate(['/login']);
+  }
+
 
 }
