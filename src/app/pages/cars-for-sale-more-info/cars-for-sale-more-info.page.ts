7986ac8c49dc43f5dd992e9ae24a8a682f12271d
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-04-20 18:22:26 +0300

    added router to cars for sale info

--- src/app/pages/cars-for-sale-more-info/cars-for-sale-more-info.page.ts
+++ src/app/pages/cars-for-sale-more-info/cars-for-sale-more-info.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cars-for-sale-more-info',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarsForSaleMoreInfoPage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
-
+  GoToHomePage(){
+    this.router.navigate(['/home-page']);
+  }
+  GoToProfile(){
+    this.router.navigate(['/profile']);
+  }
+  GoToFavorites(){
+    this.router.navigate(['/favorites']);
+  }
+  GoToLogIn(){
+    this.router.navigate(['/login']);
+  }
 }
