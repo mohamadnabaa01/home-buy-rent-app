07e8da91dc2f1018811debf858d581bf8e1e872c
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-04-22 22:27:08 +0300

    updated register.php

--- HomeCarBuyRentApp/register.php
+++ HomeCarBuyRentApp/register.php
@@ -1,20 +1,23 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers:Content-Type');
+header('Access-Control-Allow-Headers: Content-Type');
 
-include("db_info.php");
+include "db_info.php";
 
 $data = json_decode(file_get_contents("php://input"));
 
 $first_name = $data->first_name;
 $last_name = $data->last_name;
 $email_address = $data->email_address;
-$phone_number = intval($data->phone_number);
+$phone_number = $data->phone_number;
 $password = hash("sha256",$data->password);
 $user_type = $data->user_type;
 
 
 $query = $mysqli->prepare("INSERT INTO users (first_name, last_name, email_address, phone_number, password, user_type) VALUES (?, ?, ?, ?, ?, ?)");
 $query->bind_param("sssiss", $first_name, $last_name, $email_address, $phone_number, $password, $user_type);
-$query->execute(); 
+$query->execute();
+
+$json_response = json_encode($email_address);
+echo $json_response;
 ?>
