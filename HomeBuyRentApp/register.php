df27f80508757c61a0a4fcf944f000ac1a5434d9
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-05-04 14:17:15 +0300

    added alerts to register page

--- HomeBuyRentApp/register.php
+++ HomeBuyRentApp/register.php
@@ -13,11 +13,20 @@ $phone_number = $data->phone_number;
 $password = hash("sha256",$data->password);
 $user_type = $data->user_type;
 
-
-$query = $mysqli->prepare("INSERT INTO users (first_name, last_name, email_address, phone_number, password, user_type) VALUES (?, ?, ?, ?, ?, ?)");
-$query->bind_param("sssiss", $first_name, $last_name, $email_address, $phone_number, $password, $user_type);
+$query = $mysqli->prepare("SELECT email_address FROM users WHERE email_address=?");
+$query->bind_param("s", $email_address);
 $query->execute();
 
-$json_response = json_encode($email_address);
-echo $json_response;
+$array = $query->get_result();
+
+$email_address_user = $array->fetch_assoc();
+    
+if(empty($email_address_user)){
+    $query2 = $mysqli->prepare("INSERT INTO users (first_name, last_name, email_address, phone_number, password, user_type) VALUES (?, ?, ?, ?, ?, ?)");
+    $query2->bind_param("sssiss", $first_name, $last_name, $email_address, $phone_number, $password, $user_type);
+    $query2->execute();
+    $json_response = json_encode($email_address);
+    echo $json_response;
+}
+
 ?>
--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -3,6 +3,7 @@ import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { RegisterService } from 'src/app/apis/register.service';
 import { Storage } from '@ionic/storage';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -12,17 +13,36 @@ import { Storage } from '@ionic/storage';
 export class RegisterPage implements OnInit {
 
 
-  constructor(private router: Router, private service:RegisterService, private storage: Storage) { }
+  constructor(private router: Router, private service:RegisterService, private storage: Storage, private alertController: AlertController) { }
 
   ngOnInit() {
   }
  
+  async showAlertSuccess(){
+    const alert = await this.alertController.create({
+      header: 'Attention',
+      message: 'Account was created successfully',
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+  async showAlertFail(){
+    const alert = await this.alertController.create({
+      header: 'Attention',
+      message: 'Email address is already taken',
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
   public onSubmit(form: NgForm){
     const user = form.value;
     this.service.addAccount(user).subscribe(response =>{
       if(response != null){
-        localStorage.setItem('email-logged-in', String(response));
-        this.router.navigate(['/home-page']);
+        this.showAlertSuccess();
+        this.router.navigate(['/login']);
+      }
+      else{
+        this.showAlertFail();
       }
     });
   }
